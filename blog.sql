8a4b3715ddebf0fb74968217f194928eefdc644c
bzgasparKREDO <kredoteacher.bzgaspar@gmail.com>
2022-09-19 21:23:07 +0800

    sql and img final

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 19, 2022 at 03:21 PM
+-- Generation Time: Sep 19, 2022 at 03:22 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
